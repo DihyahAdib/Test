80a98c54ea14b1707cda409e98bf02c58600ff2d
DihyahAdib <112100886+DihyahAdib@users.noreply.github.com>
2024-10-11 16:50:47 -0400

    COOL cool commit 4

--- Script.js
+++ Script.js
@@ -4,3 +4,10 @@ function scrollToSection(event, sectionId) {
         behavior: 'smooth' // Smooth scrolling
     });
 }
+
+function scrollToSection(event, sectionId) {
+    event.preventDefault();
+    document.getElementById(sectionId).scrollIntoView({
+        behavior: 'smooth' 
+    });
+}
